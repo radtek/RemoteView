40cab33df8565c713f0974eb0b9389d71baaa8b0
zhangze6000 <zhangze6000@163.com>
2013-11-10 21:46:44 +0800

    系统初始化架构

---
+++ RemoteViewLib/RemoteViewLib.cpp
@@ -0,0 +1,22 @@
+// RemoteViewLib.cpp : 定义 DLL 应用程序的导出函数。
+//
+
+#include "stdafx.h"
+#include "RemoteViewLib.h"
+
+
+// 这是导出变量的一个示例
+REMOTEVIEWLIB_API int nRemoteViewLib=0;
+
+// 这是导出函数的一个示例。
+REMOTEVIEWLIB_API int fnRemoteViewLib(void)
+{
+	return 42;
+}
+
+// 这是已导出类的构造函数。
+// 有关类定义的信息，请参阅 RemoteViewLib.h
+CRemoteViewLib::CRemoteViewLib()
+{
+	return;
+}
